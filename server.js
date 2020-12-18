e65a1a2c90d609f58d288745cddec9e8016aa354
Purvesh Makode <makodepurvesh@gmail.com>
2020-12-19 03:47:12 +0530

    add static file path to server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ app = express();
 server = require('http').createServer(app);
 io = require('socket.io').listen(server);
 
+app.use(express.static(__dirname+'/public'));
+
 usernames = [];
 
 server.listen(process.env.PORT || 3000);
